2cb5f401c5848c86ce8f21be88bfda9f68268bfa
vanya <vanyakalmykov2022@mail.ru>
2024-10-23 21:11:23 +0300

    t

--- .docker/certbot/generate-certificate.sh
+++ .docker/certbot/generate-certificate.sh
@@ -2,9 +2,11 @@
 
 rm -rf /etc/letsencrypt/live/certfolder*
 
-certbot certonly --standalone --email $DOMAIN_EMAIL -d $DOMAIN_URL --cert-name=certfolder --key-type rsa --agree-tos
+#certbot certonly --standalone --email $DOMAIN_EMAIL -d $DOMAIN_URL --cert-name=certfolder --key-type rsa --agree-tos
+
+certbot certonly --webroot --agree-tos --email $DOMAIN_EMAIL --webroot-path /usr/share/nginx/html/ -d $DOMAIN_URL -d www.$DOMAIN_URL
  
 #certbot certonly --agree-tos -d $DOMAIN_URL -d *.$DOMAIN_URL --preferred-challenges dns --manual --server https://acme-v02.api.letsencrypt.org/directory
 
-cp -f /etc/letsencrypt/live/certfolder*/fullchain.pem /usr/local/nginx/conf/cert.pem
-cp -f /etc/letsencrypt/live/certfolder*/privkey.pem /usr/local/nginx/conf/key.pem
+cp -f /etc/letsencrypt/live/certfolder*/fullchain.pem /usr/share/nginx/html/cert.pem
+cp -f /etc/letsencrypt/live/certfolder*/privkey.pem /usr/share/nginx/html/key.pem
