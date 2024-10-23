bbc0ad9c0f95f8fca2469d5fcfd68881328a09da
vanya <vanyakalmykov2022@mail.ru>
2024-10-23 03:49:20 +0300

    delete tmp 443 port

--- .docker/certbot/generate-certificate.sh
+++ .docker/certbot/generate-certificate.sh
@@ -3,6 +3,7 @@
 rm -rf /etc/letsencrypt/live/certfolder*
 
 certbot certonly --standalone --email $DOMAIN_EMAIL -d $DOMAIN_URL --cert-name=certfolder --key-type rsa --agree-tos
+ 
 
 cp -f /etc/letsencrypt/live/certfolder*/fullchain.pem /etc/nginx/cert.pem
 cp -f /etc/letsencrypt/live/certfolder*/privkey.pem /etc/nginx/key.pem
