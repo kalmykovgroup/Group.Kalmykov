966c52ed1576faf5948752e556fac1f06842dede
vanya <vanyakalmykov2022@mail.ru>
2024-10-24 04:31:02 +0300

    d

--- .docker/certbot/generate-certificate.sh
+++ .docker/certbot/generate-certificate.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-if [ ! -f ${SSL_FILENAME_CHAIN} ] || [ ! -f ${SSL_FILENAME_KEY} ]; then
+if [ ! -f ${SSL_FILE_CHAIN} ] || [ ! -f ${SSL_FILE_KEY} ]; then
 
 	echo "ssl files does exist. Получение сертификатов..."
 
-	if [ -f ${SSL_FILENAME_CHAIN} ]; then
-		rm ${SSL_FILENAME_CHAIN}
+	if [ -f ${SSL_FILE_CHAIN} ]; then
+		rm ${SSL_FILE_CHAIN}
 	fi
 
-	if [ -f ${SSL_FILENAME_KEY} ]; then
-		rm ${SSL_FILENAME_KEY}
+	if [ -f ${SSL_FILE_KEY} ]; then
+		rm ${SSL_FILE_KEY}
 	fi
 
     certbot certonly --standalone --email $DOMAIN_EMAIL -d $DOMAIN_URL --cert-name=certfolder --key-type rsa --agree-tos --config-dir $SSL_DIR
 
 
-  if [ ! -f ${SSL_FILENAME_CHAIN} ] || [ ! -f ${SSL_FILENAME_KEY} ]; then
+  if [ ! -f ${SSL_FILE_CHAIN} ] || [ ! -f ${SSL_FILE_KEY} ]; then
 
 	  echo "Файлы не найдены, сертификаты не установленны!"
 
